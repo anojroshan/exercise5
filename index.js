b324a4c13ff01c6322039111995090fd06b518ec
anojroshan <73768440+anojroshan@users.noreply.github.com>
2021-10-05 10:52:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var http = require('http'); 
+
+const PORT= process.env.PORT||5000;
+var server = http.createServer(function (req, res) {  
+    if (req.url == '/') { 
+        
+
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/student") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.end();
+    }
+    else if (req.url == "/admin") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.end();
+    
+    }
+    else
+    
+        res.end('Invalid Request!');
+
+});
+
+server.listen(PORT,()=>{
+    
+console.log('Server running on port ' + PORT)
+});
